13a358b56ca24d5125d895b500be441130841d13
sbznq6skg4-wq <sbznq6skg4@privaterelay.appleid.com>
2026-09-02 16:24:52 +0500

    Implement basic bot functionality with aiogram

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+TOKEN = "8036652216:AAEou7rbZPHMTg8xHB..." # o'z tokeningni yozasan
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Test muvaffaqiyatli o'tdi! Bot ishlayapti.")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
